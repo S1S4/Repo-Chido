1c98989609c45e698fe80c5dc1591ced01a2d11c
S1S4 <Cesar_Zhiro26@hotmail.com>
2022-09-06 19:06:55 -0400

    Creada separacion de index.html, styles.css. agregado script.js, en este commit se fusionan la clase 11 y 7 al index.html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('akkakakakakakak');
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive')
+}
